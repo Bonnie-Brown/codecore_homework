674e594166218d1dd06dedb35a161d678d30bf87
Bonnie Brown <bonnie.brown@jane.app>
2022-09-18 03:20:48 +0000

    Turned the file into a script

--- box_it_script/box_it_script.js
+++ box_it_script/box_it_script.js
@@ -1,25 +1,32 @@
 
+let value = [process.argv[2]];
+
+let cometogether = value.join(",")
+
+let values = cometogether.split(",")
+
+
 // Function that takes a number as an argument that returns that number of horizontal bard as a string.
 
 function drawLine(num) {
     return '─'.repeat(num)
     
-}
+};
 
 //Write three functions: drawTopBorder, drawMiddleBorder and drawBottomBorder.
 //  Each function should take a number, return a line of length including corner pieces. 
 
 function drawTopBorder(num) {
     return '┌' + drawLine(num) + '┐'
-}
+};
 
 function drawMiddleBorder(num) {
     return '│' + drawLine(num) + '│'
-}
+};
 
 function drawBottomBorder(num) {
     return '└' + drawLine(num) + '┘'
-}
+};
 
 // Write a `drawBarsAround` function that takes a string, surrounds it with
 // vertical lines then returns it.
@@ -27,7 +34,7 @@ function drawBottomBorder(num) {
 function drawBarsAround(str) {
     return '│' + str + '│'
     
-}
+};
 
 // Write a boxIt function that takes an array of strings and returns a string where each is in a single column table. 
 // To add "new lines" to a string, use the \n special character.
@@ -36,16 +43,20 @@ function drawBarsAround(str) {
 function boxIt(arr) {
 
     for (let i = 0; i < arr.length; i++) {
+        let num = 20
         if (arr[i] === arr[0]) {
-            console.log (drawTopBorder(10) + '\n' + drawBarsAround(arr[i]) + '\n') /*+'\n' + drawMiddleBorder(10) + '\n') */ 
+            console.log (drawTopBorder(num) + '\n' + drawBarsAround(arr[i]) + '\n') 
         } else if (arr[i] !== arr[0] && arr[i] !== arr[arr.length-1]){
-            console.log(drawMiddleBorder(10)+ '\n' + drawBarsAround(arr[i]) + '\n')
+            console.log(drawMiddleBorder(num)+ '\n' + drawBarsAround(arr[i]) + '\n')
 
         } else {
-            console.log(drawMiddleBorder(10)+ '\n' + drawBarsAround(arr[i]) + '\n' + drawBottomBorder(10))
+            console.log(drawMiddleBorder(num)+ '\n' + drawBarsAround(arr[i]) + '\n' + drawBottomBorder(num))
         }
         
     }
 
-}
+};
+
+console.log(boxIt(values))
+
      
