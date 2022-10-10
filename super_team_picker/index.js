90f010d7e4f886f1bc16bc7ba084d8238c83cd31
Bonnie Brown <bonnie.brown@jane.app>
2022-10-10 18:37:52 +0000

    First draft/layout

---
+++ super_team_picker/index.js
@@ -0,0 +1,69 @@
+// Require the Express Library
+
+const express = require('express');
+const app = express();
+
+app.use(express.urlencoded({extended: true}));
+
+// Require Cookie Parser
+
+const cookieParser = require('cookie-parser')
+app.use(cookieParser())
+
+// Custom Cookie Middleware Goes Here
+
+// Require Path for express.static
+
+const path = require('path');
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Require Morgan (Logging Middleware)
+
+const logger = require('morgan');
+app.use(logger('dev'));
+
+// Routers
+
+// Home 
+
+app.get('/', (req, res) => {
+    res.render('home', {
+        title: 'Home'
+    })
+});
+
+// Cohorts
+
+app.get('/cohorts', (req, res)=>{
+    res.render('cohorts', {
+        title: 'Cohorts'
+    })
+});
+
+// New
+
+app.get('/new', (req, res) => {
+    res.render('new', {
+        title: 'New Cohorts'
+    })
+})
+
+
+
+// Set View Engine
+
+app.set('view engine', 'ejs');
+app.set('views', 'views');
+
+//Server
+
+const PORT = 3000;
+const DOMAIN = 'localhost';
+
+
+app.listen(PORT, DOMAIN, () => {
+    console.log(`Server is listening on http://${DOMAIN}: ${PORT}`)
+});
+
+
+
