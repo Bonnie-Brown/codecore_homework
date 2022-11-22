6b2b8153dcf095c8fe330900c3846f58e1af95d7
Bonnie Brown <bonnie.brown@jane.app>
2022-11-22 01:06:48 +0000

    Buttons are now disabled after being clicked and added a function to determine if the letter that was clicked is in the word to be guessed.

--- hangman/index.js
+++ hangman/index.js
@@ -7,6 +7,7 @@ const colours = ["white", "yellow", "blue", "red", "green", "black", "brown", "g
 let word = '';
 let correct = [];
 let incorrect = [];
+let guessed = [];
 
 
 // Function to Select a Word from Colours at Random
@@ -14,15 +15,42 @@ let incorrect = [];
 function selectWord(){
 
     word = colours[Math.floor(Math.random() * colours.length)]
+    
 
 }
 
 // Add Event Listener to Buttons
 
-letters = document.querySelectorAll('.btn-secondary')
+letter_buttons = document.querySelectorAll('.btn-secondary')
 
-letters.forEach(letter => {
-    letter.addEventListener('click', event =>{
-        console.log('Clicked!')
+letter_buttons.forEach(letter_button => {
+    letter_button.addEventListener('click', event =>{
+        guess(letter_button)
     })
 });
+
+function guess(letter_button) {
+    
+    // Disable the letter's button
+    letter_button.setAttribute('disabled', true)
+
+    let letter = letter_button.innerHTML.toLowerCase()
+
+    
+
+    // Determine whether or not the letter is part
+    // of the word
+
+    if (word.indexOf(letter) >= 0){
+        console.log('Success!')
+    } else {
+        console.log('Boo!')
+        console.log(letter)
+    }
+    
+    // if (!guessed.includes(letter)){
+    //     guessed.push(letter)
+    // } 
+}
+
+selectWord()
