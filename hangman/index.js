72d84a6ba849c11e6ea118121c8ebe363089d410
Bonnie Brown <bonnie.brown@jane.app>
2022-11-23 06:39:54 +0000

    Cleaned up code

--- hangman/index.js
+++ hangman/index.js
@@ -2,8 +2,8 @@ $(document).ready(function(){
 
     // Create Array of Colours to Choose From
 
-    // const colours = ["white", "yellow", "blue", "red", "green", "black", "brown", "grey", "pink", "orange", "purple", "silver", "gold", "lavender", "teal"]
-       const colours = ["white"]
+     const colours = ["white", "yellow", "blue", "red", "green", "black", "brown", "grey", "pink", "orange", "purple", "silver", "gold", "lavender", "teal"]
+    
     // Set Additional Variables
 
     let word = '';
@@ -11,7 +11,7 @@ $(document).ready(function(){
     let wrong_guesses = 0;
     let guessed_letters = [];
     let placeholder = null;
-    let alphabet = 'abcdefghijklmnopqrstuvwxyz'.split('');
+    // let alphabet = 'abcdefghijklmnopqrstuvwxyz'.split('');
 
     const winner = () => new Audio("sounds/tada.wav");
     const loser = () => new Audio("sounds/sad.wav");
@@ -37,7 +37,6 @@ $(document).ready(function(){
     function random_word() {
 
         word = colours[Math.floor(Math.random() * colours.length)]
-        console.log(word)
 
     }
 
@@ -69,6 +68,7 @@ $(document).ready(function(){
     function guess(letter_button) {
 
         // Disable the letter's button once clicked
+
         letter_button.setAttribute('disabled', true)
 
         let letter = letter_button.innerHTML.toLowerCase()
@@ -96,6 +96,7 @@ $(document).ready(function(){
 
     // When an incorrect guess is made, update the picture and check to see
     // if the user has lost the game
+
     function incorrect_guess() {
 
         wrong_guesses += 1
@@ -106,25 +107,25 @@ $(document).ready(function(){
     }
 
     // Check to see if a user has won or lost a game
+
     function game_status() {
 
         if (placeholder === word) {
 
             winner().play();
-            // setTimeout(confirm('You won!'), 200);
             setTimeout(() => { confirm('You won!') }, 1000)
             reset();
 
         } else if (wrong_guesses === max_failed_attempts) {
 
             loser().play();
-            // setTimeout(confirm('You lost!'), 200);
             setTimeout(() => { confirm('You lost!') }, 1000)
             reset();
         }
     }
 
     // Update the picture as the user guesses incorrectly
+
     function update_picture() {
 
         document.getElementById('image').src = './img/gallows' + wrong_guesses + '.jpg'
@@ -132,6 +133,7 @@ $(document).ready(function(){
     }
 
     // Update the game as the user guesses correctly
+
     function update_game() {
 
         placeholder = word.split('').map(letter => (guessed_letters.indexOf(letter) >= 0 ? letter : " _ ")).join('')
@@ -143,6 +145,7 @@ $(document).ready(function(){
     update_game();
 
     // Reset the game when the user clicks the reset button
+
     function reset() {
         wrong_guesses = 0;
         guessed_letters = [];
@@ -162,25 +165,7 @@ $(document).ready(function(){
             button.removeAttribute('disabled')
 
         });
-    }
-
-    // Play Audio
-
-   
-
-    // function play_winner_audio() {
-    //     // document.getElementById('winner').play();
-    //     winner()
-    //     setTimeout(confirm('You won!'), 1000);
-    // }
-    // function play_loser_audio() {
-
-    //     // document.getElementById('loser').play();
-    //     loser()
-    //     setTimeout(confirm(`You lost! The colour was: ${word}.`), 1000);
-
-    // }  
-    
+    }  
 })
 
 
